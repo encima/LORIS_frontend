fd1fc1d1b2f5ad3bda67a2dbe3e596a076e2d25c
encima@gmail.com <encima@gmail.com>
2013-08-06 11:15:32 +0100

    Added async viewing of Sensors from GSN

--- routes/gsn.js
+++ routes/gsn.js
@@ -1,41 +1,28 @@
 var config = require('../config'),
     fs = require('fs'),
-	xml2js = require('xml2js');
+	xml2js = require('xml2js'),
+	async = require('async');
 
 exports.vsensor = function(req, res){
-	// var files = fs.readdirSync(config.gsn_vsensor_dir, function(err, result) {
-	// 	console.log('done');
-	// });
-	var sensors = fs.readdirSync(config.gsn_vsensor_dir);
-	res.end(extractXML(sensors));
-	// return res.send(JSON.stringify(sensors));
- // return res.send(JSON.stringify(sensors));
-	
-};
 
-function extractXML(files) {
-	//change to be synchronous and wait for array!
-		var sensors = [];
-		var running = 0;
-		for(var i in files) {
-			var running = 1;
-		  var ext = files[i].split('.').pop();
-		  if(ext == 'xml' || ext == 'XML') {
-		  	console.log(files[i]);
-			var parser = new xml2js.Parser();
-		  	fs.readFile(config.gsn_vsensor_dir + '/' + files[i], function(err, data) {
-		  	    parser.parseString(data, function (err, result) {
-		  	        for(i in result) {
-		  	        	console.log(i)
-		  	        	if (typeof result[i] == 'object') {
-		  	        		console.log('')
-		  	        	}
-		  	        }
-		  	        sensors.push(result);	  	        
-		  	    });
-		  	});
-		  }
-		}
-		while(running == 1) {}
-		return JSON.stringify(sensors);
+  var i = 0;
+  var running = 1;
+  var sensors = [];
+  
+  var files = [];
+  // read in files and sfilter out xml files
+  fs.readdirSync(config.gsn_vsensor_dir).forEach(function(file) {
+  	var ext = file.split('.').pop();
+  	if (ext == 'XML' || ext == 'xml') {
+  		files.push(config.gsn_vsensor_dir + '/' + file);
+  	}
+  });
+  // create results array of filtered files that contains the contents
+  async.map(files, fs.readFile, function(err, results){
+  	var parser = new xml2js.Parser();
+  	// parse XML to JSON and send back to frontend
+  	async.map(results, parser.parseString, function(err, results) {
+  		res.end(JSON.stringify(results));  
+  	});
+  });
 };
