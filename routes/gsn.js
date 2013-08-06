c919ee17b292f477fb8912808f5d44da760cb71a
encima@gmail.com <encima@gmail.com>
2013-08-06 17:41:13 +0100

    Added dynamic loading of GSN XML files

--- routes/gsn.js
+++ routes/gsn.js
@@ -6,10 +6,35 @@ var config = require('../config'),
 exports.vsensor = function(req, res){
 
   var i = 0;
-  var running = 1;
   var sensors = [];
-  
   var files = [];
+
+  // read in files and sfilter out xml files
+  fs.readdirSync(config.gsn_vsensor_dir).forEach(function(file) {
+  	var ext = file.split('.').pop();
+  	if (ext == 'XML' || ext == 'xml') {
+  		files.push(config.gsn_vsensor_dir + '/' + file);
+  		sensors.push(file);
+  	}
+  });
+  res.render('vsensor', {'title' : config.title, 'sensors': sensors});  
+};
+
+exports.vsensor_load = function(req, res) {
+	//FIX, send back file contents!
+	var file = config.gsn_vsensor_dir + '/' + req.body.file;
+	fs.readFile(file, function(err, result) {
+		var parser = new xml2js.Parser();
+		parser.parseString(result, function(err, result) {
+			res.end(JSON.stringify(result));
+		});
+	});
+}
+
+exports.read_all_vsensors = function(req, res) {
+  var i = 0;
+  var files = [];
+
   // read in files and sfilter out xml files
   fs.readdirSync(config.gsn_vsensor_dir).forEach(function(file) {
   	var ext = file.split('.').pop();
@@ -22,7 +47,7 @@ exports.vsensor = function(req, res){
   	var parser = new xml2js.Parser();
   	// parse XML to JSON and send back to frontend
   	async.map(results, parser.parseString, function(err, results) {
-  		res.end(JSON.stringify(results));  
+  		res.end(JSON.stringify(results));	
   	});
   });
-};
+}
